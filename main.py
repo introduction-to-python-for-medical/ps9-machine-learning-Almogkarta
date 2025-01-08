1051f870e70f0165da27d2ba38cff9a9dc02612f
Almogkarta <karta.almog@gmail.com>
2025-01-08 17:54:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-import joblib
 
 parkinsons_df = pd.read_csv('parkinsons.csv')
-parkinsons_df = parkinsons_df.dropna()  # Remove rows with missing values
-
+parkinsons_df= parkinsons_df.dropna()
+parkinsons_df.head()
 selected_features = ['PPE', 'DFA']
-target = 'status'
-x = parkinsons_df[selected_features]
-y = parkinsons_df[target]
+target= ['status']
+x=parkinsons_df[selected_features]
+y=parkinsons_df[target]
 
+from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
 
-
+from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
 
-
-svc = SVC()
+from sklearn.svm import SVC
+svc = SVC ()
 svc.fit(x_train, y_train)
 
-
+from sklearn.metrics import accuracy_score
 y_pred = svc.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy:.2f}")
+print(f"Accuracy: {accuracy}")
 
+
+import joblib
 
 joblib.dump(svc, 'svc_model.joblib')
